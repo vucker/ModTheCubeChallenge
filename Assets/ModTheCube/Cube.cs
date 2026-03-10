5772d814b510ef4b35baac14c4ea1582e9ffc34b
vucker <103271312+vucker@users.noreply.github.com>
2026-03-10 22:30:13 +0300

    Смена цвета и прозрачности через время

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -5,21 +5,43 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-    public float speedRotatiom = 1f;
+    public float speed = 1f;
+    private Material material;
+    private float timeCooldown = 0f;
     void Start()
     {
         transform.position = new Vector3(5, 12, 2);
         transform.localScale = Vector3.one * 2.5f;
-        
-        Material material = Renderer.material;
-        
-        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
 
-        speedRotatiom = 20f;
+        material = Renderer.material;
+        
+        material.color = new Color(0.5f, 0.0f, 0.0f, 0.0f);
+        timeCooldown = Time.time + speed;
     }
     
     void Update()
     {
-        transform.Rotate(speedRotatiom * Time.deltaTime, 0.0f, 0.0f);
+        transform.Rotate(speed * Time.deltaTime, 0.0f, 0.0f);
+        if (Time.time >= timeCooldown)
+        {
+            ChangeColor();
+            timeCooldown = Time.time + speed;
+        }
     }
+    private void ChangeColor()
+    {
+        float channelRed = 0f, 
+            channelGreen = 0f, 
+            channelBlue = 0f,
+            channelAlpgha = 0f;
+
+        channelRed = RandFloat(channelRed);
+        channelGreen = RandFloat(channelGreen);
+        channelBlue = RandFloat(channelBlue);
+        channelAlpgha = RandFloat(channelAlpgha);
+
+        material.color = new Color(channelRed, channelGreen, channelBlue, channelAlpgha);
+
+    }
+    private float RandFloat(float channel) => channel = Random.Range(0.0f, 1.0f);
 }
