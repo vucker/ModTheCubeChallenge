7082670a07caece308292b203ec98e16f3f3c32e
vucker <103271312+vucker@users.noreply.github.com>
2026-03-10 21:51:25 +0300

    Скорость вращения

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-    
+    public float speedRotatiom = 1f;
     void Start()
     {
-        transform.position = new Vector3(3, 4, 1);
-        transform.localScale = Vector3.one * 1.3f;
+        transform.position = new Vector3(5, 12, 2);
+        transform.localScale = Vector3.one * 2.5f;
         
         Material material = Renderer.material;
         
         material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+
+        speedRotatiom = 20f;
     }
     
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        transform.Rotate(speedRotatiom * Time.deltaTime, 0.0f, 0.0f);
     }
 }
